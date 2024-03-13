86a924fc2dfec56cda0adce3d9df0bb5cda4659a
maple syrup <maple@maple.pet>
2024-03-12 23:38:49 -0300

    missed tabs in fancy clock

--- MicroHydra/apps/FancyClock.py
+++ MicroHydra/apps/FancyClock.py
@@ -120,22 +120,22 @@ def get_random_colors():
 	sat1 = random.randint(0,255)
 	sat2 = random.randint(50,255)
 
-    
-    val1 = random.randint(245,255)
-    val2 = random.randint(10,20)
-    
-    
-    
-    #convert to color565
-    ui_color = st7789py.color565(hsv_to_rgb((hue1,sat1,val1)))
-    bg_color = st7789py.color565(hsv_to_rgb((hue2,sat2,val2)))
-    lighter_color = st7789py.color565(hsv_to_rgb((hue2,max(sat2 - 5, 0),val2 + 8)))
-    darker_color = st7789py.color565(hsv_to_rgb((hue2,min(sat2 + 60, 255),max(val2 - 4,0))))
-    
-    #get middle hue
-    mid_color = mh.mix_color565(bg_color, ui_color)
-    
-    return ui_color, bg_color, mid_color, lighter_color, darker_color
+	
+	val1 = random.randint(245,255)
+	val2 = random.randint(10,20)
+	
+	
+	
+	#convert to color565
+	ui_color = st7789py.color565(hsv_to_rgb((hue1,sat1,val1)))
+	bg_color = st7789py.color565(hsv_to_rgb((hue2,sat2,val2)))
+	lighter_color = st7789py.color565(hsv_to_rgb((hue2,max(sat2 - 5, 0),val2 + 8)))
+	darker_color = st7789py.color565(hsv_to_rgb((hue2,min(sat2 + 60, 255),max(val2 - 4,0))))
+	
+	#get middle hue
+	mid_color = mh.mix_color565(bg_color, ui_color)
+	
+	return ui_color, bg_color, mid_color, lighter_color, darker_color
 
 
 kb = keyboard.KeyBoard()
@@ -276,80 +276,80 @@ while True:
 	tft.hline(x_pos-2, y_pos-1, time_width + 20, lighter_color)
 	tft.hline(x_pos-2, y_pos+48, time_width + 20, darker_color)
 
-    
-    if moving_right:
-        x_pos += 1
-    else:
-        x_pos -= 1
-        
-    if moving_up:
-        y_pos -= 1
-    else:
-        y_pos +=1
-    
-    
-    #y_collision
-    if y_pos <= 1:
-        y_pos = 1
-        moving_up = False
-        ui_color, bg_color, mid_color, lighter_color, darker_color = get_random_colors()
-        red_color = mh.color565_shiftred(mid_color)
-        batt_level = batt.read_level()
-        
-    elif y_pos >= 87:
-        y_pos = 87
-        moving_up = True
-        ui_color, bg_color, mid_color, lighter_color, darker_color = get_random_colors()
-        red_color = mh.color565_shiftred(mid_color)
-        batt_level = batt.read_level()
-        
-        
-    #x_collision
-    if x_pos <= 0:
-        x_pos = 0
-        moving_right = True
-        ui_color, bg_color, mid_color, lighter_color, darker_color = get_random_colors()
-        red_color = mh.color565_shiftred(mid_color)
-        batt_level = batt.read_level()
-        
-    elif x_pos >= 224 - time_width:
-        x_pos = 224 - time_width
-        moving_right = False
-        ui_color, bg_color, mid_color, lighter_color, darker_color = get_random_colors()
-        red_color = mh.color565_shiftred(mid_color)
-        batt_level = batt.read_level()
-    
-    
-    
-    
-    #refresh bg on 5 mins
-    if minute != old_minute and minute % 5 == 0:
-        old_minute = minute
-        tft.fill(0)
-        
-    #keystrokes and backlight
-    pressed_keys = kb.get_pressed_keys()
-    if pressed_keys != prev_pressed_keys: # some button has been pressed
-        if "GO" in pressed_keys:
-            tft.fill(0)
-            tft.sleep_mode(True)
-            blight.duty_u16(0)
-            reset()
-        current_bright = bright_peak
-    elif current_bright != min_bright:
-        current_bright -= bright_step
-        if current_bright < min_bright:
-            current_bright = min_bright
-        blight.duty_u16(min(max_bright,current_bright))
-        
-        
-    prev_pressed_keys = pressed_keys
-    
-    if "SPC" in pressed_keys:
-        current_bright = bright_peak
-        time.sleep_ms(1)
-    else:
-        time.sleep_ms(70)
+	
+	if moving_right:
+		x_pos += 1
+	else:
+		x_pos -= 1
+		
+	if moving_up:
+		y_pos -= 1
+	else:
+		y_pos +=1
+	
+	
+	#y_collision
+	if y_pos <= 1:
+		y_pos = 1
+		moving_up = False
+		ui_color, bg_color, mid_color, lighter_color, darker_color = get_random_colors()
+		red_color = mh.color565_shiftred(mid_color)
+		batt_level = batt.read_level()
+		
+	elif y_pos >= 87:
+		y_pos = 87
+		moving_up = True
+		ui_color, bg_color, mid_color, lighter_color, darker_color = get_random_colors()
+		red_color = mh.color565_shiftred(mid_color)
+		batt_level = batt.read_level()
+		
+		
+	#x_collision
+	if x_pos <= 0:
+		x_pos = 0
+		moving_right = True
+		ui_color, bg_color, mid_color, lighter_color, darker_color = get_random_colors()
+		red_color = mh.color565_shiftred(mid_color)
+		batt_level = batt.read_level()
+		
+	elif x_pos >= 224 - time_width:
+		x_pos = 224 - time_width
+		moving_right = False
+		ui_color, bg_color, mid_color, lighter_color, darker_color = get_random_colors()
+		red_color = mh.color565_shiftred(mid_color)
+		batt_level = batt.read_level()
+	
+	
+	
+	
+	#refresh bg on 5 mins
+	if minute != old_minute and minute % 5 == 0:
+		old_minute = minute
+		tft.fill(0)
+		
+	#keystrokes and backlight
+	pressed_keys = kb.get_pressed_keys()
+	if pressed_keys != prev_pressed_keys: # some button has been pressed
+		if "GO" in pressed_keys:
+			tft.fill(0)
+			tft.sleep_mode(True)
+			blight.duty_u16(0)
+			reset()
+		current_bright = bright_peak
+	elif current_bright != min_bright:
+		current_bright -= bright_step
+		if current_bright < min_bright:
+			current_bright = min_bright
+		blight.duty_u16(min(max_bright,current_bright))
+		
+		
+	prev_pressed_keys = pressed_keys
+	
+	if "SPC" in pressed_keys:
+		current_bright = bright_peak
+		time.sleep_ms(1)
+	else:
+		time.sleep_ms(70)
 
 
 
